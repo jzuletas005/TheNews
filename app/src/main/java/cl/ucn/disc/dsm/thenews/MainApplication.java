cedd3aae70c4a48b8da1ce9bf9bf3e9ceb7c370c
jzuletas005 <46382682+jzuletas005@users.noreply.github.com>
2020-01-17 02:35:20 -0300

    pref(Main):update MainApplication.java
    Actualizacion de MainApplication

--- app/src/main/java/cl/ucn/disc/dsm/thenews/MainApplication.java
+++ app/src/main/java/cl/ucn/disc/dsm/thenews/MainApplication.java
@@ -2,6 +2,8 @@ package cl.ucn.disc.dsm.thenews;
 
 import android.app.Application;
 import androidx.appcompat.app.AppCompatDelegate;
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.jakewharton.threetenabp.AndroidThreeTen;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,10 +21,16 @@ public class MainApplication extends Application {
    */
   @Override
   public void onCreate() {
+
     super.onCreate();
+    log.debug("Initializing ..");
 
     // Day and Night support
-    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY);
+    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+
+    // Facebook fresco
+    Fresco.initialize(this);
+    AndroidThreeTen.init(this);
 
     log.debug("Initializing: Done.");
   }
